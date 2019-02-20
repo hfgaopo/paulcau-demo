2cdecd547867f2197f88ec2885d053ba36e76d60
gaopo <gaopo@ppdai.com>
2019-02-20 20:26:06 +0800

    快速排序

--- src/main/java/com/gaw/demo/sort/QuickSort.java
+++ src/main/java/com/gaw/demo/sort/QuickSort.java
@@ -8,36 +8,21 @@ import java.util.Arrays;
  */
 public class QuickSort {
 
-    private static int transfer(int highIndex, int lowIndex, int baseIndex, int[] source) {
-        if (lowIndex >= highIndex) {
-            return highIndex;
-        }
-        if (source[lowIndex] <= source[baseIndex]) {
-            lowIndex++;
-        }
-        if (source[highIndex] >= source[baseIndex]) {
-            highIndex--;
-        }
-        if (source[highIndex] < source[lowIndex]) {
-            int temp = source[highIndex];
-            source[highIndex] = source[lowIndex];
-            source[lowIndex] = temp;
-        }
-        return transfer(highIndex, lowIndex, baseIndex, source);
-    }
-
     private static void quickSort(int highIndex, int lowIndex, int[] source) {
         //int response = transfer(highIndex, lowIndex, lowIndex, source);
         int left = highIndex;
         int right = lowIndex;
+        //基准数
+        int base = source[right];
+        //排序
         while (true) {
             if (highIndex <= lowIndex) {
                 break;
             }
-            if (source[highIndex] >= source[right]) {
+            if (source[highIndex] >= base) {
                 highIndex--;
             }
-            if (source[lowIndex] <= source[right]) {
+            if (source[lowIndex] <= base) {
                 lowIndex++;
             }
             if (source[highIndex] < source[lowIndex]) {
@@ -46,16 +31,21 @@ public class QuickSort {
                 source[lowIndex] = temp;
             }
         }
-        int temp = source[right];
+        //基准数归位
         source[right] = source[lowIndex];
-        source[lowIndex] = temp;
-        quickSort(lowIndex, right, source);
-        quickSort(left, lowIndex + 1, source);
-
+        source[lowIndex] = base;
+        //分治处理
+        if (lowIndex > right) {
+            quickSort(lowIndex - 1, right, source);
+        }
+        if (highIndex < left) {
+            quickSort(left, lowIndex + 1, source);
+        }
     }
 
     public static void main(String[] args) {
-        int[] source = {212, 341, 5646, 7, 45, 75, 89, 4, 57, 42, 67, 456, 9, 231, 42, 74, 326, 2134, 26761, 23797, 23434, 234, 3545, 24, 75, 9, 32465, 9965, 2367, 3621, 3, 678};
+        //int[] source = {212, 341, 5646, 7, 45, 75, 89, 4, 57, 42, 67, 456, 9, 231, 42, 74, 326, 2134, 26761, 23797, 23434, 234, 3545, 24, 75, 9, 32465, 9965, 2367, 3621, 3, 678};
+        int[] source = {212, 341, 5646, 7, 45, 75, 89, 4, 57, 42, 67};
         System.out.println(Arrays.toString(source));
         quickSort(source.length - 1, 0, source);
         System.out.println(Arrays.toString(source));
